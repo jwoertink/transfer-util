f1a51410d0fe803ccdec5579a6bec41c2e950abf
jwoertink <jeremywoertink@gmail.com>
2008-09-19 15:11:48 -0700

    Found all the templates, going to print them to the screen. Cleaned up some variables so they make more sense now. Removed some code not being used.

--- scrape.rb
+++ scrape.rb
@@ -2,15 +2,16 @@ require 'rubygems'
 require 'open-uri'
 require 'mechanize'
 require 'logger'
+require 'rtml_parser'
 
 #setup variables
 #requires Yahoo Small Business account.
 @url = 'http://smallbusiness.yahoo.com/ecommerce/'
 @store_list = 'http://store.yahoo.com/index4.html'
-@login = ''
-@password = ''
-@security_key = ''
-@store_id = ''
+@login = 'vwdjeremy'
+@password = 'mar56jux8'
+@security_key = 'super88dizl'
+@store_id = 'yhst-35296642127615'
 
 #setup the browser
 @browser = WWW::Mechanize.new do |agent|
@@ -35,7 +36,7 @@ end
 
   puts "Going to login"
   #Find the login form and fill in the input values.
-  #This returns some odd page with a single link on it?????
+  #This returns some odd page with a single link on it.
   temp_page = @login_page.form_with(:name => 'login_form') do |form|
     form['login'] = @login
     form['passwd'] = @password
@@ -65,138 +66,135 @@ end
   #We are looking for a particular store
   if page.links.text(@store_id)
     puts "Found #{@store_id}"
-    @store_manager = page.links.text(@store_id).click
+    @store_front = page.links.text(@store_id).click
   else
     puts "Could not find #{@store_id}"
     exit
   end
 
-	puts "Looking at #{@store_manager.uri.to_s}"
-  puts 'Looking for Store Editor'
-  @store_manager.links.each do |link|
-		case link.text
-		when "Store Editor"
-		  puts "Found store editor link"
-      @store_index = @store_manager.links.text("Store Editor").click
-		when "Store Manager"
-      puts "Found store manager link, going to store manager"
-      @store_manager = @store_manager.links.text("Store Manager").click
-      sleep 2
-      puts 'Going to Store Editor'
-      @store_index = @store_manager.links.text("Store Editor").click
-		else
-			puts "Couldn't find store manager."
-		end
-  end
+  #Found the store, now we need to get to the store manager
+  puts 'Looking for Manager'
 
   #NOTE:
   # Currently the form hangs up on the security key form. Only in Windows....... (vista)
   # It just locks up compeltely, not sure why. As long as there isn't a security key form then we are ok.
-  # seems on a *nix system it is ok. 
-  if @store_manager.forms.length > 0 && @store_manager.uri.to_s.include?("edit.secure.yahoo.com")
-    puts "Uh oh, need to enter in the security key"
-		sleep 1
-    @store_manager.forms.each do |form|
+  # seems on a *nix system it's ok.
+  if @store_front.forms.length > 0 && @store_front.uri.to_s.include?("edit.secure.yahoo.com")
+    puts "Need to verify security key."
+    sleep 1
+    @store_front.forms.each do |form|
       case form.name
       when 'login_form'
         form['passwd'] = @password
         puts 'submitting login_form'
         @temp_page = form.click_button
+        sleep 4
         break
       when 'a'
         form['passwd'] = @security_key
         puts 'submitting a form'
         @temp_page = form.click_button
+        sleep 4
         break
       else
         puts form.name
       end
     end
-    
+
     #not displaying errors.... There may be an issue with how the form is submitted.
-    #it locked me out after "supposedly" entering the wrong SK 3 times.
-    #Verified it was the correct one. There might be a time delay, and that is what is causing 
+    #You will be locked out if the security key is entered wrong 3 times.
+    #If the URL has edit.secure.yahoo.com then it failed.
     if @temp_page.uri.to_s.include?("edit.secure.yahoo.com")
       puts 'Failed security key'
       @temp_page.search("ul font li") do |error|
         puts "*** " + error
       end
-      #exit
-      @temp_page.search("html") do |html|
-        puts html.inner_html
-      end
+      exit
     else
-      puts 'Passed security key'
-			puts "We are in #{@temp_page.uri.to_s}"
-      @store_index = @temp_page
+      puts 'Security key verified.'
+      puts "We are in #{@temp_page.uri.to_s}"
+      @store_variable_page = @temp_page
     end
 
     puts "waiting on redirect..."
     sleep 4
   else
-    @store_index = @store_manager.links.text("Store Editor").click
+    #The store variable page could be one of a few different pages
+    # 1. An ad for a store upgrade
+    # 2. The Store manager
+    # THIS SECTION NOT IN USE....
+    #@store_front.links.each do |link|
+    #  case link.text
+    #  when "Store Editor"
+    #    puts "Found store editor link"
+    #    @store_index = link.click
+    #  when "Store Manager"
+    #    puts "Found store manager link, going to store manager"
+    #    @store_manager = link.click
+    #    sleep 2
+    #    puts 'Going to Store Editor'
+    #    @store_index = @store_manager.links.text("Store Editor").click
+    #  else
+    #    puts "Couldn't find store manager."
+    #  end
+    #end
+    puts "something went wrong."
+    exit
   end
 
-  puts 'Inside index'
-
-  @store_index.links.each do |link|
+  @store_variable_page.links.each do |link|
     if link.text.eql?("Store Manager")
-			puts "uh oh, we weren't even in the manager"
-			@new_page = link.click
-			sleep 4
-			puts "we are now at #{@new_page.uri.to_s}"
+      puts @store_variable_page.title
+      @new_page = link.click
+      sleep 4
+      puts "we are now at #{@new_page.uri.to_s}"
 
-			@new_page.links.each do |l|
-				if l.text.eql?("Store Editor")
-					puts "found the real index page."
-					@true_index = l.click
-					sleep 4
-					puts "we are now at #{@true_index.uri.to_s}"
-				end
-			end
-		end
+      @new_page.links.each do |l|
+        if l.text.eql?("Store Editor")
+          puts "found the real index page."
+          @true_index = l.click
+          sleep 4
+          puts "we are now at #{@true_index.uri.to_s}"
+        end
+      end
+    end
   end
-	puts 'Going to try to get to the contents page'
-	@true_index.links.each do |link|
-		if link.text.eql?("Contents")
-			puts 'on index, found contents!'
-			@store_contents = link.click
-			puts 'going to look for the templates page'
-			sleep 4
-			@store_contents.links.each do |l|
-				if l.text.eql?("Templates")
-					puts 'gathering templates....'
-					@store_templates = l.click
-					sleep 2
-				end
-			end
-		elsif link.text.eql?("Templates")
-			puts "on contents, found templates!"
-			@store_templates = link.click
-			sleep 2
-		end
-	end
+  puts 'Going to try to get to the contents page'
+  @true_index.links.each do |link|
+    if link.text.eql?("Contents")
+      puts 'on index, found contents!'
+      @store_contents = link.click
+      puts 'going to look for the templates page'
+      sleep 4
+      @store_contents.links.each do |l|
+        if l.text.eql?("Templates")
+          puts 'gathering templates....'
+          @store_templates = l.click
+          sleep 2
+        end
+      end
+    elsif link.text.eql?("Templates")
+      puts "on contents, found templates!"
+      @store_templates = link.click
+      sleep 2
+    end
+  end
 
-puts 'ok, so where am I at now? ....'
-	@store_templates.links.each do |link|
-				puts link.text
-	end
-
+  puts 'We should have the templates. Trying to list them now....'
 
   sleep 2
-	unless @store_templates.nil?
-					puts "we are in #{@store_templates.uri.to_s}"
-					template_page = Hpricot(open(@store_templates.uri.to_s))
+  # As long as we are on the templates page,
+  # we can grab all of them and start the next process.
+  unless @store_templates.nil?
+    @templtates = @store_templates.search('/html/body/p/table[2]/tbody/tr[2]/td[2]//a')
 
-					if template_page.at("title").inner_html.eql?("Yahoo! Store Editor")
-									template_page.search('a') do |link|
-													puts link
-									end
-					else
-									puts "The Title for this page"
-									puts template_page.at("title").inner_html
-					end
-	end
+    @templates.each do |hpricot_link|
+      #template_page = @browser.click(hpricot_link)
+      #RTMLParser::RTML.parse_page(template_page)
+      puts hpricot_link
+    end
+
+  end
 
   puts "done."
 end
